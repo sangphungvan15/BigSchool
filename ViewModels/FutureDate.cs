99ad1dd604990cfe787899b4e5ddef8ea3448613
sangphungvan15 <44025778+sangphungvan15@users.noreply.github.com>
2021-07-10 14:07:12 +0700

    Add project files.

---
+++ ViewModels/FutureDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime;
+            var isValid = DateTime.TryParseExact(Convert.ToString(value), "M/dd/yyyy", CultureInfo.CurrentCulture, System.Globalization.DateTimeStyles.None, out dateTime);
+            return (isValid && dateTime > DateTime.Now);
+        }
+    }
+}
