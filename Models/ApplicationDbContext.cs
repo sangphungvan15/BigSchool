2ed44362c006b89043b5e5c36d1137a550eef5c6
sangphungvan15 <44025778+sangphungvan15@users.noreply.github.com>
2021-07-11 19:47:54 +0700

    update 1

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace BigSchool.Models
         public DbSet<Course> Course { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Attendance> AttenDances { get; set; }
+        public DbSet<Following> Followings { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -26,6 +27,14 @@ namespace BigSchool.Models
             modelBuilder.Entity<Attendance>()
                 .HasRequired(a => a.Course)
                 .WithMany()
+                .WillCascadeOnDelete(false);
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
                 .WillCascadeOnDelete(false);
             base.OnModelCreating(modelBuilder);
         }
